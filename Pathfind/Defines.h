f125466cabe7c363c449bdd6b48eb477cbbfedd1
Celania <garet@gmx.net>
2013-10-11 14:35:38 +0200

    adding general defines

---
+++ Pathfind/Defines.h
@@ -0,0 +1,11 @@
+/* Globally defined variables*/
+
+typedef unsigned int uint;
+
+typedef short int16;
+typedef int int32;
+typedef long long int64;
+
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
